eaf69b6d1a51addba9363624a7f4d624cc0f1148
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2024-04-15 21:27:56 +0530

    connected the prisma with the webhooks

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
 // here we can configure the routes
-
+  publicRoutes:["/api/webhooks(.*)"]
 });
 
 export const config = {
