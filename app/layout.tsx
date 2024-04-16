3c1d508b77a21b821692812056fc062124697eb5
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2024-04-16 20:31:04 +0530

    added Sonner package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
-import { dark } from "@clerk/themes";
+
 import { ThemeProvider } from "@/components/theme-provider";
-
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +16,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider appearance={{baseTheme:dark}}>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
         <body className={inter.className}>
           <ThemeProvider
@@ -28,8 +28,8 @@ export default function RootLayout({
             forcedTheme="dark"
             storageKey="StreamEZ"
           >
+            <Toaster theme="light" position="bottom-center" />
             {children}
-
           </ThemeProvider>
         </body>
       </html>
